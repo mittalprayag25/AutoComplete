2dd680061154a10f648ee30c4b6c599c8464192f
Prayag <prayag.mittal@singtel.com>
2021-11-14 10:39:26 +0800

    addon functionality

--- src/component/autocomplete/AutoComplete.js
+++ src/component/autocomplete/AutoComplete.js
@@ -3,14 +3,15 @@ import './AutoComplete.css';
 
 const AutoComplete = (props) => {
     const [dictionary, setDictionary] = useState([]);
-    const { options, onChange, value, onTextChange } = props;
+    const { options, onChange, value, onTextChange, multiValues, onRemove } = props;
 
     useEffect(() => {
         setDictionary(options);
     }, [options]);
 
     const handleChange = (e) => {
-        const selectedOptions = Array.from(e.target.selectedOptions, option => option.value)
+        const selectedOptions = Array.from(e.target.selectedOptions, option => option.value);
+        console.log("selectedOptions", selectedOptions);
         onChange(selectedOptions);
     }
 
@@ -29,10 +30,27 @@ const AutoComplete = (props) => {
     return (
         <div className={"mainContainer"}>
             <div className={"searchableContainer"}>
-                <input className={"searchInput"}
-                    type='text'
-                    placeholder="Search Country"
-                    onKeyUp={(e) => searchText(e.target.value)} />
+                <div className="multiSelectContainer">
+                    {multiValues.length > 0 ?
+                        <div className={"selectedOptionContainer"}>
+                            {
+                                multiValues.map((item, key) => {
+                                    return (
+                                        <span className={"selectedOptions"}>{`${item}`}
+                                            <span className="cross" onClick={() => onRemove(item)}>
+                                                x
+                                            </span>
+                                        </span>
+                                    )
+                                })
+                            }
+                        </div> : null
+                    }
+                    <input className={"searchInput"}
+                        type='text'
+                        placeholder="Search.."
+                        onKeyUp={(e) => searchText(e.target.value)} />
+                </div>
                 {dictionary.length > 0 && value ?
                     <select
                         className={"selectContainer"}
--- src/component/parent/ParentComponent.js
+++ src/component/parent/ParentComponent.js
@@ -4,11 +4,29 @@ import './ParentStyle.css';
 
 const ParentComponent = () => {
     const [options, setOptions] = useState(['India', 'Singapore', 'China', 'America', 'Australia', 'Kenya', 'South Afirca', 'Iraq', 'Canada']);
-    const [value, setValue] = useState(null);
+    const [value, setValue] = useState();
+    const [selectedOptions, setSelectedOptions] = useState([]);
 
     const onChange = (data) => {
-        console.log(data);
+        const arr = [...selectedOptions, ...data];
+        setSelectedOptions(arr.filter(unique));
     };
+
+    const unique = (value, index, self) => {
+        return self.indexOf(value) === index;
+    };
+
+    const onRemove = (item) => {
+        console.log("ONREMOVE", item);
+        const arr = arrayRemoveElement([...selectedOptions], item);
+        setSelectedOptions(arr.filter(unique));
+    };
+
+    const arrayRemoveElement = (arr, item) => {
+        return arr.filter((ele) => {
+            return ele !== item;
+        })
+    }
 
     return (
         <div className={"parentContainer"}>
@@ -16,6 +34,8 @@ const ParentComponent = () => {
                 value={value}
                 options={options}
                 onChange={onChange}
+                multiValues={selectedOptions}
+                onRemove={onRemove}
                 onTextChange={setValue} />
 
         </div>
